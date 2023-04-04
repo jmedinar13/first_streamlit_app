01be6c89ed755c55fce06a285b900a032b305a01
jmedinar13 <129540135+jmedinar13@users.noreply.github.com>
2023-04-04 14:35:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+streamlit.stop()
+
 # create the repeatable code block (called a function)
 def get_fruityvice_data(This_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choce)
